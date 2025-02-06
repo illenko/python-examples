ef3ee472c0d5c2ff3bb5003c3366e34c02ee81d6
illenko <kostiaillenko@gmail.com>
2025-02-06 18:33:14 +0200

    Add line chart ant scatter

---
+++ charts/line_chart.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+
+variance = [1, 2, 4, 8, 16, 32, 64, 128, 256]
+bias_squared = [256, 128, 64, 32, 16, 8, 4, 2, 1]
+total_error = [x + y for x, y in zip(variance, bias_squared)]
+
+print(f"total_error: {total_error}")
+
+xs = [i for i, _ in enumerate(variance)]
+
+print(f"xs: {xs}")
+
+plt.plot(xs, variance, 'g-', label='variance')
+plt.plot(xs, bias_squared, 'r-.', label='bias^2')
+plt.plot(xs, total_error, 'b:', label='total error')
+
+plt.legend(loc=9)
+plt.xlabel("model complexity")
+plt.title("The Bias-Variance Tradeoff")
+plt.show()
